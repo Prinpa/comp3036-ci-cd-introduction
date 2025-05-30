c38b2b090ffa1bb036f07a5a88de2ca5ba9e2f4f
Pat Dowd <pa77y04@gmail.com>
2025-05-30 10:31:51 +1000

    pipeline test

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ export default function Home() {
     <main>
       <h1>My Awesome Blog</h1>
       <p>Welcome to a CI/CD-ready blog app!</p>
+      <p>Welcome to a CI/CD-ready blog app!</p>
+      <p>Welcome to a CI/CD-ready blog app!</p>
+      <p>Welcome to a CI/CD-ready blog app!</p>
     </main>
   );
 }
